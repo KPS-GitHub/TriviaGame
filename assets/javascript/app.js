36ff92fcaae33eb270ef9bbc2176f21dad18285a
Kennan <kennan.smith1@gmail.com>
2018-05-03 15:45:54 -0600

    added a little pseudo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 // Variables
-// -counters
+//  -counters
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-// -question array
+//  -question array
 var questionArray = [
     {   question: "Question 1",
         answers: ["option 1", "option 2", "option 3", "option 4"],
@@ -24,15 +24,16 @@ $(document).ready(function() {
  
 // new game button (reset)
 $("#newGame").on("click", function() {
-    console.log("registered new game click");
     // clear main div
     $("#main").empty();
-    console.log("emptied #main");
+    // div for timer
+    $("#main").append("<div id='timer'></div>");
     // for loops to generate questions and answer options
     // generate questions
     for (var i = 0; i < questionArray.length; i++) {
-        var quizQuestion = $("<h3>");
+        var quizQuestion = $("<div>");
         quizQuestion.attr("id", "question" + i);
+        quizQuestion.attr("class", "row");
         quizQuestion.text(questionArray[i].question);
         $("#main").append(quizQuestion);
         $("#main").append("\n");
@@ -40,16 +41,26 @@ $("#newGame").on("click", function() {
         for (var j = 0; j < questionArray[i].answers.length; j++) {
             // make the input tag
             var quizAnswerInput = $("<input type='radio'></input>");
-            quizAnswerInput.attr("name", "option" + (j+1));
-            quizAnswerInput.attr("id", "option" + (j+1));
+            quizAnswerInput.attr("name", "question" + (i));
             quizAnswerInput.text("question"); // questionArray[i].answers[j]
             // make the label tag
-            var quizAnswerLabel = $("<label for='option" + (j+1) + "'>" + questionArray[i].answers[j] + "</label>");
+            var quizAnswerLabel = $("<label for='option" + (i) + "'>" + questionArray[i].answers[j] + "</label>");
             console.log("quizAnswerLabel: " + quizAnswerLabel);
+            // render label and input for the created question in the appropriate question id div
             $("#question" + i).append(quizAnswerLabel);
             $("#question" + i).append(quizAnswerInput);
             $("#question" + i).append("<br>");
         }
     }
 })
+
+$("#submitAnswers").on("click", function() {
+    // stop timer process
+    
+    // total correct, incorrect, and unanswered questions
+
+    // wipe #main div and render scores + any relevant messages
+
+})
+
 });
