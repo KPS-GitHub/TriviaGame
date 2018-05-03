dc791a2ac27d5f79ed8e1b7cd9e948e4969f2002
Kennan <kennan.smith1@gmail.com>
2018-05-03 08:44:23 -0600

    basic html setup and starting to work through js

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+// Variables
+// -counters
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+// -question array
+var questions = [
+    { question: "Question 1",
+        answers = { 
+            option1: "option 1",
+            option2: "option 2",
+            option3: "option 3",
+            option4: "option 4"
+        },
+        correctAnswer: option1,
+    },
+    { question: "Question 2",
+        answers = { 
+            option1: "option 1",
+            option2: "option 2",
+            option3: "option 3",
+            option4: "option 4"
+        },
+        correctAnswer: option1,
+    },
+    { question: "Question 3",
+        answers = { 
+            option1: "option 1",
+            option2: "option 2",
+            option3: "option 3",
+            option4: "option 4"
+        },
+        correctAnswer: option1,  
+    }
+];
+
+// Functionality
+
+// new game button (reset)
+$("#newGame").on("click", function() {
+    // UNFINISHED LINE !!! --- !!! --- !!!
+    // fill in html("")
+    $("#main").empty().html("");
+})
