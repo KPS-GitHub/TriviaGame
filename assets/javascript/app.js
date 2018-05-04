9bdc9343c51ec6b94bfe29b4ddc84fb185ee3cc8
Kennan <kennan.smith1@gmail.com>
2018-05-03 21:33:37 -0600

    minor additions, still basically zero functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,6 +28,10 @@ $("#newGame").on("click", function() {
     $("#main").empty();
     // div for timer
     $("#main").append("<div id='timer'></div>");
+    // create and start timeout, call timesUp() fnc when timer finishes
+    var quizTime = setTimeout(function() {
+        timesUp();
+    }, 60000);
     // for loops to generate questions and answer options
     // generate questions
     for (var i = 0; i < questionArray.length; i++) {
@@ -42,7 +46,10 @@ $("#newGame").on("click", function() {
             // make the input tag
             var quizAnswerInput = $("<input type='radio'></input>");
             quizAnswerInput.attr("name", "question" + (i));
+            quizAnswerInput.attr("value", "question" + (i));
             quizAnswerInput.text("question"); // questionArray[i].answers[j]
+            console.log("quizAnswerInput: " + quizAnswerInput);
+            console.log("quizAnswerInput value: " + quizAnswerInput.value);
             // make the label tag
             var quizAnswerLabel = $("<label for='option" + (i) + "'>" + questionArray[i].answers[j] + "</label>");
             console.log("quizAnswerLabel: " + quizAnswerLabel);
@@ -54,12 +61,20 @@ $("#newGame").on("click", function() {
     }
 })
 
+// alerts to check that radio buttons are working and see their value
+$('#main input').on('change', function() {
+    console.log("registered change");
+    alert($('input[name=question1]:checked', '#main').val()); 
+ });
+
 $("#submitAnswers").on("click", function() {
     // stop timer process
-    
+    clearTimeout(quizTime);
     // total correct, incorrect, and unanswered questions
-
-    // wipe #main div and render scores + any relevant messages
+    // for (var i = 0; i < questionArray.length; i++) {
+    //     if ($('input[name=radioName]:checked', '#myForm').val())
+    // }
+    // wipe #main div and render scores + any relevant messages (press new quiz to start over)
 
 })
 
